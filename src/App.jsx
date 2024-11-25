0978df0d7e7bb2a54ab71fec59bfa897cc20d083
Juan Esteban Camargo <juanescacha@hotmail.com>
2024-11-24 22:43:09 -0500

    build: add base assets and base code

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-
-
-
-
-
 const App = () => {
-
-
-  return (
-    <>
-
-
-
-
-    </>
-  )
+	return (
+		<main>
+			<h1 className="text-5xl font-bold text-orange-500">
+				Welcome to Zentry
+			</h1>
+		</main>
+	)
 }
 
 export default App
